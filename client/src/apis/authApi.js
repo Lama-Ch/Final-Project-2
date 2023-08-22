77949389d3dd5050ed09264e1c016c0a3ce58bf4
Lama Chebib <lama.chebib@gmail.com>
2023-08-22 16:41:16 -0400

    auth

--- client/src/apis/authApi.js
+++ client/src/apis/authApi.js
@@ -9,6 +9,15 @@ export const singInService = (crendentials) => {
 		body: JSON.stringify(crendentials),
 	});
 };
+export const getCurrentUserService = (authToken) => {
+	return fetch(`${API_URL}/auth/current-user`, {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: `Bearer ${authToken}`,
+		},
+	});
+};
 
 export const signUpService = (data) => {
 	return fetch(`${API_URL}/auth/signup`, {
