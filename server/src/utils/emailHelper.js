8621962019ce9fc2af76e862ab6a7718c722aa36
Lama Chebib <lama.chebib@gmail.com>
2023-09-02 19:55:45 -0400

    email

--- server/src/utils/emailHelper.js
+++ server/src/utils/emailHelper.js
@@ -8,27 +8,27 @@ exports.sendOrderConfirmationEmail = (orderDetails) => {
 			timeStyle: 'short',
 		}
 	);
-
+//for sending emails
 	const transporter = nodemailer.createTransport({
 		service: 'Gmail',
 		auth: {
-			user: process.env.EMAIL_USER_NAME,
-			pass: process.env.EMAIL_PASSWORD,
+			user: process.env.EMAIL_USER_NAME, // Your email username
+			pass: process.env.EMAIL_PASSWORD,// Your email password
 		},
 	});
 
 	const mailOptions = {
-		from: `Food Service <${process.env.EMAIL_USER_NAME}>`,
+		from: `Food EATS <${process.env.EMAIL_USER_NAME}>`,
 		to: {
 			name: orderDetails.fullname,
 			address: orderDetails.email,
 		},
-		subject: 'Order Confirmation',
-		html: `<p>Thank you for your order! Your order details:</p>
-           <p>Total Price: $${orderDetails.total}</p>
+		subject: 'Order Confirmation',// Email subject
+		html: `<p>Thank you for choosing Food Eats
+		 Your order details:</p>your order on it's way
            <p>Order Date & Time: ${formattedDate}</p>`,
 	};
-
+ // Send the email
 	transporter.sendMail(mailOptions, (error, info) => {
 		if (error) {
 			console.error('Error sending email:', error);
