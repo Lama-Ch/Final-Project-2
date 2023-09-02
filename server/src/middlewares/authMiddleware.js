495524374133e1757e70bc9be522bb20125955aa
Lama Chebib <lama.chebib@gmail.com>
2023-09-02 19:54:43 -0400

    auth

--- server/src/middlewares/authMiddleware.js
+++ server/src/middlewares/authMiddleware.js
@@ -1,20 +1,23 @@
+//'jsonwebtoken' library
 const jwt = require('jsonwebtoken');
 
 const authMiddleware = (req, res, next) => {
+	  // Get the Authorization header from the request
 	const authHeader = req.headers.authorization;
-
+// Check if the Authorization header is missing or doesn't start with 'Bearer '
 	if (!authHeader || !authHeader.startsWith('Bearer ')) {
 		return res
 			.status(401)
 			.json({ error: 'Please login to perfrom this action.' });
 	}
-
+// Extract the token from the Authorization header by removing 'Bearer '
 	const token = authHeader.replace('Bearer ', '');
 
 	try {
+		// JWT secret key
 		const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
 
-		// Attach the user data to the request for further use
+		// Attach the user ID to the request for later use
 		req.authUserId = decoded.userId;
 		next();
 	} catch (error) {
