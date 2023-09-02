2f4b47a56de272fa9bacf5b2d644b43f52c28841
Lama Chebib <lama.chebib@gmail.com>
2023-09-02 19:54:18 -0400

    local

--- client/src/utils/localStorage.js
+++ client/src/utils/localStorage.js
@@ -1,12 +1,12 @@
-//storage mechanism for the browser, to save all keys and data for the browser.
+// Function to save an authentication token to localStorage
 export const saveAuthToken = (token) => {
 	localStorage.setItem('authToken', token);
 };
-
+// Function to get the stored authentication token from localStorage
 export const getStoredAuthToken = () => {
 	return localStorage.getItem('authToken');
 };
-
+// Function to remove the stored authentication token from localStorage
 export const removeStoreAuthToken = () => {
 	localStorage.removeItem('authToken');
 };
