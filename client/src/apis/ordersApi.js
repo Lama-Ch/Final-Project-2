8257ea0f518b62a98411b4657ad6de059e5715c0
Lama Chebib <lama.chebib@gmail.com>
2023-09-02 19:53:13 -0400

    order

--- client/src/apis/ordersApi.js
+++ client/src/apis/ordersApi.js
@@ -1,5 +1,5 @@
 import { API_URL } from '../utils/constants';
-
+// Function to fetch details about a specific order by its ID
 export const orderByIdService = (orderId, authToken) => {
 	const headers = {
 		'Content-Type': 'application/json',
@@ -7,17 +7,20 @@ export const orderByIdService = (orderId, authToken) => {
 	if (authToken) {
 		headers['Authorization'] = `Bearer ${authToken}`;
 	}
+	// Send a GET request to the API endpoint for the specified order ID
 	return fetch(`${API_URL}/orders/${orderId}`, {
 		method: 'GET',
 		headers: headers,
 	});
 };
-
+// Function to create a new order
 export const createOrderService = (data, authToken) => {
+	// Initialize headers with the Content-Type and the provided authentication token
 	const headers = {
 		'Content-Type': 'application/json',
 		Authorization: `Bearer ${authToken}`,
 	};
+	// Send a POST request to the API endpoint to create a new order
 	return fetch(`${API_URL}/orders/`, {
 		method: 'POST',
 		headers: headers,
