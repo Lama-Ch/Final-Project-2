dec94614b78fb32ac4c2860548a2b3573cdd7e74
Lama Chebib <lama.chebib@gmail.com>
2023-09-02 19:55:23 -0400

    user

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-	username: { type: String, unique: true },
-	fullname: String,
-	email: { type: String, unique: true },
-	password: String,
+	username: { type: String, unique: true },// User's username (unique)
+	fullname: String,// User's full name
+	email: { type: String, unique: true },// User's email (unique)
+	password: String,// User's password
 });
 
 const User = mongoose.model('User', userSchema);
