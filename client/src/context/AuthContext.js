2b281b4b330fcd6f8e1b1c74a84e742088256550
Lama Chebib <lama.chebib@gmail.com>
2023-09-02 19:53:36 -0400

    auth

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -2,15 +2,17 @@ import React, { createContext, useContext, useState } from 'react';
 
 const AuthContext = createContext();
 
+//component called AuthProvider
 export const AuthProvider = ({ children }) => {
 	const [user, setUser] = useState(null); // User object
 	const [token, setToken] = useState(null); // JWT token
 
+// Function to set user data and token when user logs in
 	const setUserLoggedIn = (userData, authToken) => {
 		setUser(userData);
 		setToken(authToken);
 	};
-
+// Function to log out the user by resetting user and token
 	const logout = () => {
 		setUser(null);
 		setToken(null);
