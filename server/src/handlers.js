a298da8e1c8aaeb42887d453350a5c2647708995
Lama Chebib <lama.chebib@gmail.com>
2023-09-24 18:19:47 -0400

    handlers

--- server/src/handlers.js
+++ server/src/handlers.js
@@ -157,7 +157,7 @@ exports.getFoodsList = (req, res) => {
 		foodType: foodType,
 		postedBy: { $ne: authUserId },
 	})
-	.select('name description foodType isVegetarian price imageUrl') // Select the fields to include in the response
+	
 		.populate({ path: 'postedBy', select: ['username', 'fullname', 'email'] })
 		.then((result) => {
 			return res.status(201).json({ result });
@@ -175,7 +175,7 @@ exports.getFoodDetailById = (req, res) => {
 	const authUserId = req.authUserId;
 
 	Food.findById(foodId)
-	.select('name description foodType isVegetarian price imageUrl') // Select the fields to include in the response
+	
 		.populate({ path: 'postedBy', select: ['username', 'fullname', 'email'] })
 		.then((result) => {
 			if (!result) {
