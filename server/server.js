4a4c338dce1ad462f1bd23c19a781df6a85b804e
Lama Chebib <lama.chebib@gmail.com>
2023-09-25 16:00:11 -0400

    server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const path = require("path");
 const cors = require("cors"); // Import the cors module
 const app = express();
 const dotenv = require("dotenv");
+const multer = require('multer');
 const handlers= require ("./src/handlers");
 const authMiddleware = require ("./src/middlewares/authMiddleware");
 const optionalAuthMiddleware = require ("./src/middlewares/optionalAuthMiddleware");
@@ -9,6 +11,23 @@ const optionalAuthMiddleware = require ("./src/middlewares/optionalAuthMiddlewar
 dotenv.config();
 const PORT = process.env.PORT || 8000;
 
+const assetsDirectory = path.join(__dirname, 'uploads/');
+
+// Multer configuration for handling file uploads
+const storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+	  // Set the destination folder where uploaded images will be stored
+	  cb(null, 'uploads/');
+	},
+	filename: function (req, file, cb) {
+	  // Set the filename for the uploaded image
+	   // Replace spaces with underscores and keep the extension
+	   const sanitizedFileName = file.originalname.replace(/\s+/g, '_');
+	  cb(null, Date.now() + '-' + sanitizedFileName);
+	},
+  });
+  
+  const upload = multer({ storage: storage });
 
 // Initialize the DB connection
 require("./src/configs/dbClient")
@@ -26,6 +45,7 @@ require("./src/configs/dbClient")
     next();
   })
   .use(express.json())
+  .use('/uploads', express.static(assetsDirectory))
 
   // Auth 
   .post("/api/auth/signup", handlers.signup)
@@ -34,7 +54,7 @@ require("./src/configs/dbClient")
   .get('/api/auth/current-user', authMiddleware, handlers.getCurrentUser)
  
   // My Food and my listing
-  .post("/api/foods", authMiddleware, handlers.postNewFood)
+  .post("/api/foods", authMiddleware, upload.single('foodImage'), handlers.postNewFood)
   .get("/api/foods/my-listings/:foodType",authMiddleware,handlers.getMyListedFoods)
 
   // Get Foods Listing and Food Detail
